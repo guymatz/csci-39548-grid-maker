409cbeba831a43d107b04859e30938374b3f5284
Guy Matz <gmatz@matz.org>
2022-06-17 12:14:46 -0400

    added addR method, changed alerts to console.log

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 // Declare global variables
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
+
+let table = document.getElementById("grid");
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+    let row = table.insertRow();
+    if (numCols == 0) {
+        numCols = 1;
+    }
+    for (let i=0; i < numCols; i++) {
+        let newCell = row.insertCell();
+        newCell.onclick = function(event) {
+            this.style.backgroundColor = colorSelected;
+            console.log(event);
+        }
+    }
+    console.log("Clicked Add Row " + row + " to " + table); // Replace this line with your code.
+    numRows++;
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+    console.log("Clicked Add Col"); // Replace this line with your code.
 }
+
 
 // Remove a row
 function removeR() {
-    alert("Clicked Remove Row"); // Replace this line with your code.
+    console.log("Clicked Remove Row"); // Replace this line with your code.
 }
 
 // Remove a column
 function removeC() {
-    alert("Clicked Remove Col"); // Replace this line with your code.
+    console.log("Clicked Remove Col"); // Replace this line with your code.
 }
 
-// Set global variable for selected color
+-// Set global variable for selected color
 function selectColor(){
     colorSelected = document.getElementById("selectedColorId").value;
     console.log(colorSelected);
@@ -31,15 +46,15 @@ function selectColor(){
 
 // Fill all uncolored cells
 function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+    console.log("Clicked Fill All Uncolored"); // Replace this line with your code.
 }
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+    console.log("Clicked Fill All"); // Replace this line with your code.
 }
 
 // Clear all cells
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+    console.log("Clicked Clear All"); // Replace this line with your code.
 }
